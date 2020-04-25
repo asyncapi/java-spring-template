10ed38d240d8fb0592fe29192d2a368781bb2833
Semen Tenishchev <tenischev.semen@gmail.com>
2020-04-26 01:38:50 +0300

    feat: add descriptions as javadocs over generated code

--- partials/CommonPublisher.java
+++ partials/CommonPublisher.java
@@ -9,6 +9,10 @@ public interface PublisherService {
 
     {% for channelName, channel in asyncapi.channels() %}
         {% if channel.hasSubscribe() %}
+    {% if channel.description() or channel.subscribe().description() %}/**{% for line in channel.description() | splitByLines %}
+     * {{line | safe}}{% endfor %}{% for line in channel.subscribe().description() | splitByLines %}
+     * {{line | safe}}{% endfor %}
+     */{% endif %}
     @Gateway(requestChannel = "{{channelName | camelCase}}OutboundChannel")
     void {{channel.subscribe().id() | camelCase}}(String data);
         {% endif %}
