04491d27f653a3b588c66e326477bcb7586cbf76
Semen Tenishchev <tenischev.semen@gmail.com>
2020-04-30 19:17:41 +0300

    feat: add opposite site generation mode

---
+++ hooks/opositeSideGenerator.js
@@ -0,0 +1,20 @@
+module.exports = register => {
+    register('generate:before', generator => {
+        console.log(generator);
+        if (generator.templateParams && generator.templateParams['generateOppositeSide'] === true) {
+            console.log("Opposite side generation mode is enabled");
+            const asyncapi = generator.asyncapi;
+            for (let [key, value] of Object.entries(asyncapi.channels())) {
+                let publish = value._json.publish;
+                value._json.publish = value._json.subscribe;
+                value._json.subscribe = publish;
+                if (!value._json.subscribe) {
+                    delete value._json.subscribe;
+                }
+                if (!value._json.publish) {
+                    delete value._json.publish;
+                }
+            }
+        }
+    });
+};
