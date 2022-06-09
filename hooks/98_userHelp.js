6417ab0b6e819f82891f9f2331a6c5b24c921e5e
Semen <tenischev.semen@gmail.com>
2022-06-09 12:07:41 -0700

    Update hooks/98_userHelp.js
    use one string for help message
    
    Co-authored-by: Lukasz Gornicki <lpgornicki@gmail.com>

--- hooks/98_userHelp.js
+++ hooks/98_userHelp.js
@@ -11,12 +11,11 @@ function checkForAnonymousSchemaObjects(generator) {
         }
     });
     if (anonymousPresent) {
-        console.log('Following AnonymousSchemaN classes were generated in DTO classes:');
-        anonymousFileNames.forEach(name => console.log(name));
-        console.log('This may be a result of explicit (composition, inheritance, array items) Schema Object definition e.g.');
-        const schemaWithAnonymous =
-`  schemas:
-    NamedObject:
+  console.log(`Following AnonymousSchema classes were generated in DTO classes:
+${anonymousFileNames.toString()}\n
+This may be a result of explicit (composition, inheritance, array items) Schema Object definition e.g.\n
+    schemas:
+      NamedObject:
       type: object
         properties:
           field:
@@ -25,29 +24,24 @@ function checkForAnonymousSchemaObjects(generator) {
               type: object #Anonymous object
               properties:
                 field:
-                  type: string`;
-        const messageWithAnonymous =
-`  messages:
-    Message:
-      payload:
-        type: object #Anonymous object
-        properties:`;
-        console.log(schemaWithAnonymous);
-        console.log('OR');
-        console.log(messageWithAnonymous);
-        console.log('Please move such elements to child of "schemas:" to define proper names. ' +
-            'If changing of data model is not possible, you may use "$id" to set name e.g.');
-        const objectWithId =
-`        properties:
-          field:
-            type: array
-            items:
-              $id: ArrayElement #Name of object
-              type: object
-              properties:
-                field:
-                  type: string`;
-        console.log(objectWithId);
+                  type: string
+\nOR\n
+    messages:
+      Message:
+        payload:
+          type: object #Anonymous object
+          properties:\n
+Please move such elements to child of "schemas:" to define proper names.
+If changing of data model is not possible, you may use "$id" to set name e.g.\n
+    properties:
+      field:
+        type: array
+        items:
+          $id: ArrayElement #Name of object
+          type: object
+          properties:
+            field:
+              type: string`);
     }
 }
 
