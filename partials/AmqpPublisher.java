de6040eb768e6a6749ba8dc623b3a28c89e95304
VaishnaviNandakumar <vaishoo.nk@gmail.com>
2023-06-11 19:59:40 +0530

    Added publisher service

---
+++ partials/AmqpPublisher.java
@@ -0,0 +1,38 @@
+{% macro amqpPublisher(asyncapi, params) %}
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+{% for channelName, channel in asyncapi.channels() %}
+    {%- if channel.hasSubscribe() %}
+        {%- for message in channel.subscribe().messages() %}
+import {{params['userJavaPackage']}}.model.{{message.payload().uid() | camelCase | upperFirst}};
+        {%- endfor -%}
+    {% endif -%}
+{% endfor %}
+
+
+@Service
+public class PublisherService {
+    @Autowired
+    private RabbitTemplate template;
+
+    {% for channelName, channel in asyncapi.channels() %}
+    @Value("${amqp.{{- channelName -}}.exchange}")
+    private String {{channelName}}Exchange;
+
+    @Value("${amqp.{{- channelName -}}.routingKey}")
+    private String {{channelName}}RoutingKey;
+    {% endfor %}
+
+    {% for channelName, channel in asyncapi.channels() %}
+    {%- set schemaName = channel.subscribe().message().payload().uid() | camelCase | upperFirst %}
+    public void update{{schemaName}}(){
+        {{schemaName}} {{channelName}}Payload = new {{schemaName}}();
+        template.convertAndSend({{channelName}}Exchange, {{channelName}}RoutingKey,  {{channelName}}Payload);
+    }
+    {% endfor %}
+}
+
+{% endmacro %}
