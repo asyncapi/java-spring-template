155d620ad385a635240b8550e47f5372cf946135
Semen <tenischev.semen@gmail.com>
2023-09-05 01:11:28 +0300

    add pom.xml generation

--- tests/parameters.test.js
+++ tests/parameters.test.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const Generator = require('@asyncapi/generator');
+const {existsSync} = require("fs");
 const { readFile } = require('fs').promises;
 
 const MAIN_TEST_RESULT_PATH = path.join('tests', 'temp', 'integrationTestResult');
@@ -31,6 +32,55 @@ describe('integration tests for generated files under different template paramet
         for (const index in expectedFiles) {
             const file = await readFile(path.join(outputDir, expectedFiles[index]), 'utf8');
             expect(file).toMatchSnapshot();
+        }
+    });
+
+    it('should generate maven build', async() => {
+        const outputDir = generateFolderName();
+        const params = {"maven": true};
+        const kafkaExamplePath = './mocks/kafka.yml';
+
+        const generator = new Generator(path.normalize('./'), outputDir, { forceWrite: true, templateParams: params });
+        await generator.generateFromFile(path.resolve('tests', kafkaExamplePath));
+
+        const expectedFiles = [
+            '/pom.xml'
+        ];
+        const notExpectedFiles = [
+            '/build.gradle',
+            '/gradle.properties'
+        ];
+        for (const index in expectedFiles) {
+            const file = await readFile(path.join(outputDir, expectedFiles[index]), 'utf8');
+            expect(file).toMatchSnapshot();
+        }
+        for (const index in notExpectedFiles) {
+            expect(existsSync(path.join(outputDir, notExpectedFiles[index]))).toBeFalsy();
+        }
+    });
+
+
+    it('should generate gradle build', async() => {
+        const outputDir = generateFolderName();
+        const params = {"maven": false};
+        const kafkaExamplePath = './mocks/kafka.yml';
+
+        const generator = new Generator(path.normalize('./'), outputDir, { forceWrite: true, templateParams: params });
+        await generator.generateFromFile(path.resolve('tests', kafkaExamplePath));
+
+        const notExpectedFiles = [
+            '/pom.xml'
+        ];
+        const expectedFiles = [
+            '/build.gradle',
+            '/gradle.properties'
+        ];
+        for (const index in expectedFiles) {
+            const file = await readFile(path.join(outputDir, expectedFiles[index]), 'utf8');
+            expect(file).toMatchSnapshot();
+        }
+        for (const index in notExpectedFiles) {
+            expect(existsSync(path.join(outputDir, notExpectedFiles[index]))).toBeFalsy();
         }
     });
 });
