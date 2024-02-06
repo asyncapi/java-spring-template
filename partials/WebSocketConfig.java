1520fc39d72cc67c0d5127e55c672753006ae596
Semen <tenischev.semen@gmail.com>
2024-02-07 01:48:24 +0200

    update config

--- partials/WebSocketConfig.java
+++ partials/WebSocketConfig.java
@@ -1,13 +1,25 @@
 {% macro wsConfig(asyncapi, params) %}
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 import javax.annotation.processing.Generated;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Generated(value="com.asyncapi.generator.template.spring", date="{{''|currentTime }}")
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+public class WebSocketConfig extends WebSocketMessageBrokerConfigurer {
+
+    {% for serverName, server in asyncapi.servers() %}
+    @Value("${ws.server.{{serverName}}}")
+    private String {{serverName}}Url;
+    {% endfor %}
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -17,7 +29,21 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat");
-        registry.addEndpoint("/chat").withSockJS();
+        {% for serverName, server in asyncapi.servers() %}
+        registry.addEndpoint(getPath({{serverName}}Url));
+        registry.addEndpoint(getPath({{serverName}}Url)).withSockJS();
+        {% endfor %}
+    }
+
+    private String getPath(String serverURL) {
+        String path;
+        try {URI uri = null;
+            uri = new URI(serverURL);
+            path = uri.getPath();
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+        return path;
     }
 }
+{% endmacro %}
